f92a1ba63d73dd0d5ea7b4c0f4f65f55c3ed7b18
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-10-12 13:47:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
-const form = document.getElementById('registration-form');
-const feedbackDiv = document.getElementById('form-feedback');
-form.addEventListener('submit', function () {
-  event.preventDefault();
 
 
-  // validation variable
-  const isValid = true;
-  const messages = ['Username must be at least 3 characters long', 'Please enter a valid email address.', 'Password must be at least 6 characters long.']
-  //username validating
-  const usernameInput = document.getElementById('username');
-  trim();console.log(trimmedusernameInput);
-  if (usernameInput.value.length < 3) {
-    isValid = false + messages.push([0]);
-    return;
+  const form = document.getElementById('registration-form');
+  const feedbackDiv = document.getElementById('form-feedback');
+  form.addEventListener('submit', function () {
+    event.preventDefault();
+
+
+    // validation variable
+    const isValid = true;
+    const messages = ['Username must be at least 3 characters long', 'Please enter a valid email address.', 'Password must be at least 6 characters long.']
+    //username validating
+    const usernameInput = document.getElementById('username');
+    trim(); console.log(trimmedusernameInput);
+    if (usernameInput.value.length < 3) {
+      isValid = false + messages[0];
+      return;
+    } else {
+
+    }
+
+    // email validation
+    const userEmail = document.getElementById('email');
+    trim(); console.log(trimmeduserEmail);
+    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(userEmail.value)) {
+      isValid = false + messages[1];
+    } else {
+
+    }
+
+    // password  validation
+    const userPassword = document.getElementById('password');
+    trim(); console.log(trimmeduserPassword);
+    if (userPassword.value.length < 8) {
+      isValid = false + messages[2];
+    } else {
+
+    }
+
+    form.submit();
+     //displaying feedback
+     feedbackDiv.style.display = "block";
+  if (isValid = true) {
+    feedbackDiv.textContent = 'Registration successfu', feedbackDiv.style.color = "#28a745";
   } else {
-
+   feedbackDiv.innerHTML(messages.join(br)), feedbackDiv.style.color = "#dc3545";
   }
+ });
+  });
 
-  // email validation
-  const userEmail = document.getElementById('email');
-      trim(); console.log(trimmeduserEmail);
-  if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(userEmail.value)) {
-    isValid = false + messages.push([1]);
-  } else {
-
-  }
-
-  // password  validation
-  const userPassword = document.getElementById('password');
-      trim(); console.log(trimmeduserPassword);
-  if (userPassword.length < 8) {
-    isValid = false + messages.push([2]);
-  } else {
-
-  }
-  
-  form.submit();
-});
-
-feedbackDiv.style.display = "block";
-if (isValid = true) {
-  feedbackDiv.textContent = 'REgistration successful!', feedbackDiv.style.color = "#28a745";
-} else {
-  feedbackDiv.style.color = "#dc3545"
-}
-});
+ 
+ 
